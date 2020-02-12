784c9b9379fc9a1f9311091d983c0c96e51489a4
Ous Bayaa <bayaaous@gmail.com>
2020-02-11 22:55:57 -0800

    added wheel, dblclick and resize

--- js/index.js
+++ js/index.js
@@ -19,3 +19,26 @@ document.querySelectorAll('div p').forEach(el => {
         el.style.color = 'royalblue';
     });
 });
+
+// doubleclick
+const changeImg = document.querySelector('div img');
+changeImg.addEventListener("dblclick", () => {
+  changeImg.src="https://images.unsplash.com/photo-1504321946642-8f661bf96ff0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60";
+});
+
+//resize
+window.addEventListener("resize", () => {
+    funLogo.style.display = "none";
+  });
+
+//copy
+const copyText = document.getElementsByTagName('body')[0];
+copyText.addEventListener('copy',(e) => {
+    alert("don't copy me!");
+    e.preventDefault();
+});
+
+//scroll
+copyText.addEventListener('wheel',(e) => {
+    copyText.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+});
