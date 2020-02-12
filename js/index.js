f0ea85a32108e332825d8f959cc1ccb94b843458
Ous Bayaa <bayaaous@gmail.com>
2020-02-11 19:05:53 -0800

    added styling to h1, h4, P's

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 // Your code goes here
+//mouseover
+const funLogo = document.querySelector('.logo-heading');
+funLogo.addEventListener("mouseover", () => {
+    funLogo.style.color = "dodgerblue";
+});
+
+//mouseenter
+document.querySelectorAll('div h2, h4').forEach(el => {
+    el.addEventListener('mouseenter', () => {
+        el.style.background = 'dodgerblue';
+        el.style.border = '5px dotted crimson';
+    });
+});
+
+//mouseleave
+document.querySelectorAll('div p').forEach(el => {
+    el.addEventListener('mouseleave', () => {
+        el.style.color = 'royalblue';
+    });
+});
